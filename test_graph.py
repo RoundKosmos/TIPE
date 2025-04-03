8570f3b53bd3191bb31c0551071823b57c64f1f3
RoundKosmos <129363260+RoundKosmos@users.noreply.github.com>
2025-04-03 12:49:28 +0200

    Create test_graph.py

---
+++ test_graph.py
@@ -0,0 +1,15 @@
+"""prog en classe ?
+trouvé sur google scholar :
+                    -> [université de Lyon](https://math.univ-lyon1.fr/irem/Formation_ISN/formation_parcours_graphes/largeur/3_python1.html)
+                    -> NetworkX : Network Analysis with Python by Salvatore Scellato
+                    -> [Site de recherche en python](http://www.jmlr.org/papers/v21/19-187.html)
+                    -> [Recherche qui programme les grafs en python](https://ieeexplore.ieee.org/abstract/document/9402076)
+                    """
+
+
+class Graphe :
+  def __init__(self, voisins:list, val:str) : #voisins est la liste de tous les noeuds adjacents et val le nom du noeud
+    self.voisins = voisins
+    self.val = val
+
+  def 
