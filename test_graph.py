1322395468529d7cce5109e9586723907f64ff8c
RoundKosmos <129363260+RoundKosmos@users.noreply.github.com>
2025-04-10 12:50:34 +0200

    Update test_graph.py

--- test_graph.py
+++ test_graph.py
@@ -1,15 +1,35 @@
-"""prog en classe ?
-trouvé sur google scholar :
-                    -> [université de Lyon](https://math.univ-lyon1.fr/irem/Formation_ISN/formation_parcours_graphes/largeur/3_python1.html)
-                    -> NetworkX : Network Analysis with Python by Salvatore Scellato
-                    -> [Site de recherche en python](http://www.jmlr.org/papers/v21/19-187.html)
-                    -> [Recherche qui programme les grafs en python](https://ieeexplore.ieee.org/abstract/document/9402076)
-                    """
+class Noeuds :
+  def __init__(self, voisins:[Noeuds], nom:str) : 
+    """voisins est la liste de tous les noeuds adjacents
+    nom le nom du noeud """
+    self.voisins = voisins
+    self.name = nom
+    self.degre = len(voisins)
 
+  def name_neighbor(self):
+    """renvoie le nom de tous les voisins du noeud"""
+    return [n.name for n in self.voisins]
+
+  def new_nods(self, nod):
+    """ nod est un Noeud
+    ajoute un nouveau noeud parmi les voisins"""
+    self.voisins.append(nod)
+    self.degre += 1
 
 class Graphe :
-  def __init__(self, voisins:list, val:str) : #voisins est la liste de tous les noeuds adjacents et val le nom du noeud
-    self.voisins = voisins
-    self.val = val
+  def __init__(self, noeuds:[Noeuds]) : 
+    """noeuds est une liste de Noeuds
+    nb est le nombre de noeuds"""
+    self.nb = len(noeuds)
+    self.noeuds = noeuds
 
-  def 
+  
+  def nods_degs(self) :  """
+  renvoie tout les noeuds composant le graphe avec leur degré (Noeuds, int) """
+    nods = []
+    for n in self.noeuds :
+      nods.append( (n, n.degre))
+    return nods
+        
+  def cor_pearson(self) : #Calcule le degré de corrélation de Pearson du graphe
+    
