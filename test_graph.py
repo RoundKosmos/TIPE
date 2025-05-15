4e6d1ec414406f52cd0e70f101d411660eae7813
RoundKosmos <129363260+RoundKosmos@users.noreply.github.com>
2025-05-15 12:56:44 +0200

    Update test_graph.py

--- test_graph.py
+++ test_graph.py
@@ -1,20 +1,28 @@
 class Noeuds :
-  def __init__(self, voisins:[Noeuds], nom:str) : 
+  def __init__(self, voisins:[], nom:str, ligne = None) : 
     """voisins est la liste de tous les noeuds adjacents
     nom le nom du noeud """
     self.voisins = voisins
     self.name = nom
     self.degre = len(voisins)
+    self.ligne = ligne
 
   def name_neighbour(self):
     """renvoie le nom de tous les voisins du noeud"""
     return [n.name for n in self.voisins]
 
-  def new_neighbour(self, nod):
+  def new_neighbour(self, moi, nod):
     """ nod est un Noeud
     ajoute un nouveau noeud parmi les voisins"""
-    self.voisins.append(nod)
-    self.degre += 1
+    if nod.name not in moi.name_neighbour() :
+        self.voisins.append(nod)
+        self.degre += 1
+        self.ligne.app_n(nod)
+        
+    #reciprocite de la relation voisin
+    if moi.name not in nod.name_neighbour() :
+        nod.new_neighbour(nod, moi)
+    
 
 class Graphe :
   def __init__(self, noeuds:[Noeuds]) : 
@@ -26,30 +34,33 @@ class Graphe :
   def app_n(self, noeud:Noeuds) :
     """Rajoute a la liste de noeuds composant le graphe noeud"""
     self.noeuds.append(noeud)
+    self.nb += 1
   
-  def nods_degs(self) :  """
-  renvoie tout les noeuds composant le graphe avec leur degré (Noeuds, int) """
+  def nods_degs(self) :
+    """renvoie tout les noeuds composant le graphe avec leur degré (Noeuds, int) """
     nods = []
     for n in self.noeuds :
-      nods.append( (n, n.degre))
+      nods.append((n.name, n.degre))
     return nods
         
   #def cor_pearson(self) : #Calcule le degré de corrélation de Pearson du graphe
 
 
 if __name__ == "__main__" :
-  Shibuya = Noeuds([Omote_sando], shibuya)
-  Omote_sando = Noeuds([Shibuya, Gaiemmae], omote_sando)
-  Gaiemmae = Noeuds([Omote_sando,Aoyama_itchome], gaiemmae)
-  Aoyama_itchome = Noeuds([Gaiemmae,Akasaka_mitsuke], aoyama_itchome)
-  Akasaka_mitsuke = Noeuds([Aoyama_itchome], akasaka_mitsuke)
-  Ginza = Graphe([Shibuya, Omote_sando, Gaiemmae, Aoyama_itchome, Akasaka_mitsuke])
+  Ginza = Graphe([])
+  Shibuya = Noeuds([], "Shibuya", Ginza)
+  Omote_sando = Noeuds([Shibuya], "Omote_sando", Ginza)
+  Gaiemmae = Noeuds([Omote_sando], "Gaiemmae", Ginza)
+  Aoyama_itchome = Noeuds([Gaiemmae], "Aoyama_itchome", Ginza)
+  Akasaka_mitsuke = Noeuds([Aoyama_itchome], "Akasaka_mitsuke", Ginza)
+  ligne = [Shibuya, Omote_sando, Gaiemmae, Aoyama_itchome, Akasaka_mitsuke]
+  for n in ligne :
+      Ginza.app_n(n)
   
   print(Gaiemmae.name_neighbour())
   print(Ginza.nods_degs())
-  Tameike_sanno = Noeuds([Akasaka_mitsuke], tameike_sanno)
-  Akasaka_mitsuke.new_neighbour(Tameike_sanno)
+  Tameike_sanno = Noeuds([Akasaka_mitsuke], "Tameike_sanno", Ginza)
+  Akasaka_mitsuke.new_neighbour(Akasaka_mitsuke, Tameike_sanno)
   
-  print(Akasaka_mistune.name_neighbour())
+  print(Akasaka_mitsuke.name_neighbour())
   print(Ginza.nods_degs())
-    
